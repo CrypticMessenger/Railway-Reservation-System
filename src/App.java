567e0b0f90587b8b84226e7a1a6f6cbf6514a6c5
CrypticMessenger <2020csb1072@iitrpr.ac.in>
2022-10-27 23:45:51 +0530

    added sql-java interface

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -20,27 +25,51 @@ public class App {
         return conn;
     }
 
-    public int getCount(String SQL) {
-        int count = 0;
+    public void getResultSet(Connection conn, String query) {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
 
-        try (Connection conn = connect();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(SQL)) {
-            rs.next();
-            count = rs.getInt(1);
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1)
+                    System.out.print("  ");
+                System.out.print(rsmd.getColumnName(i));
+            }
+            System.out.println("");
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1)
+                        System.out.print("  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(columnValue + " ");
+                }
+                System.out.println("");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
         }
 
-        return count;
     }
 
     public static void main(String[] args) throws Exception {
         App app = new App();
-        // app.connect();
 
-        int ans = app.getCount("SELECT count(*) FROM classes");
+        Connection conn = app.connect();
+
+        File file = new File(
+                "D:\\Desktop\\Multi-Thread_sample\\Multi-Thread_sample\\testSubject\\Experiment1\\client\\query.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        while ((st = br.readLine()) != null && !(st.equals("FINISH"))) {
+            System.out.println(st);
+            app.getResultSet(conn, st);
+        }
+        br.close();
+
+        // int ans = app.getCount("SELECT count(*) FROM classes");
         System.out.println("Hello, World!");
-        System.out.println(ans);
+        // System.out.println(ans);
     }
 }
